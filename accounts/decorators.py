10c7fe91444fda90fdd7e8ea195b29a3842f9d85
denisyao1 <60019356+denisyao1@users.noreply.github.com>
2020-07-19 20:19:52 +0200

    Add files via upload

---
+++ accounts/decorators.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+
+def unauthenticated_user(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_func
+
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'Admin':
+            return view_func(request, *args, **kwargs)
+        if group == 'Customer':
+            return redirect('user_page')
+        return HttpResponse('You not allowed to navigate through with website')
+
+    return wrapper_func
