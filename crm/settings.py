09c15ff3a7513534e726a837dbe19361753fa473
denisyao1 <60019356+denisyao1@users.noreply.github.com>
2020-07-20 00:27:33 +0200

    config settings and Whitenoise

--- crm/settings.py
+++ crm/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,8 +116,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/images/'
+
 STATIC_URL = '/docs/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'docs'
